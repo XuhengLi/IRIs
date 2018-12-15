6fe2fd62ac0b7900100a834418a39be8101159f7
pousT <zy16373@gmail.com>
2018-12-14 22:20:23 -0500

    test suite setup

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 TEST=tests
 file=$(ls $TEST)
-./src/iris.native -l $1> source.llvm
+./src/iris.native -l tests/pass_test_inputint.ir> source.llvm
 llc source.llvm -o source.s
 clang source.s src/inputint.o -o main
 rm source.s source.llvm
