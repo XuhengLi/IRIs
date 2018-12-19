92bc830d76e2eaeb62cfbe6d8d259f083e7797ff
Xuheng Li <xl2784@columbia.edu>
2018-12-19 16:53:52 -0500

    delete junks

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-
-./src/iris.native tests/test_bin_search.ir> source.llvm
-llc source.llvm -o source.s
-
-clang source.s src/inputint.o src/inputfile.o src/lib/*.o src/cmd.o src/inputstring.o src/sendmail.o src/inputfloat.o -o main
-rm source.s source.llvm
-./main
